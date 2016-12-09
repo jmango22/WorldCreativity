b2d6159ea8f03d64cc8d19df01d20ab159546fa1
jonmengo <mengje@mtc.byu.edu>
2016-12-09 10:16:46 -0700

    loading of timeline working

--- timeline.js
+++ timeline.js
@@ -4,6 +4,7 @@
 
 //console.log("original file: "+json);
 
+/*
 for (var chapter in json.chapters)
 {
     console.log(json.chapters[chapter].title);
@@ -14,16 +15,15 @@ for (var chapter in json.chapters)
             console.log("    "+json.chapters[chapter].events[event].dates[date].month+"/"+json.chapters[chapter].events[event].dates[date].day+"/"+json.chapters[chapter].events[event].dates[date].year);
         }
 
-        for( var location in json.chapters[chapter].events[event].locations) {
-            for( var country in json.chapters[chapter].events[event].locations[location].countries) {
-                console.log("    "+json.chapters[chapter].events[event].locations[location].countries[country].value);
-            }
-            for( var city in json.chapters[chapter].events[event].locations[location].cities) {
-                console.log("    "+json.chapters[chapter].events[event].locations[location].cities[city].value);
-            }
+        for( var country in json.chapters[chapter].events[event].locations.countries) {
+             console.log("    "+json.chapters[chapter].events[event].locations.countries[country]);
+        }
+        for( var city in json.chapters[chapter].events[event].locations.cities) {
+             console.log("    "+json.chapters[chapter].events[event].locations.cities[city]);
         }
     }
 }
+*/
 
 google.charts.load('upcoming', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
@@ -53,13 +53,51 @@ function drawTimeLine() {
     // DOM element where the Timeline will be attached
     var container = document.getElementById('visualization');
 
+    var options = {};
+    var items = new vis.DataSet(options);
+
+    var id = 1;
+    var text = "";
+    var dateStart = null;
+    var dateEnd = null;
+    var group = 0;
+
+    for (var chapter in json.chapters)
+    {
+         console.log(json.chapters[chapter].title);
+	 group = group+1;
+         for ( var event in json.chapters[chapter].events )
+         {
+            console.log("  "+json.chapters[chapter].events[event].name);
+	    text = json.chapters[chapter].events[event].name;
+            for( var date in json.chapters[chapter].events[event].dates) {
+                console.log("    "+json.chapters[chapter].events[event].dates[date].month+"/"+json.chapters[chapter].events[event].dates[date].day+"/"+json.chapters[chapter].events[event].dates[date].year);
+		if(dateStart === null) {
+		    dateStart = new Date(json.chapters[chapter].events[event].dates[date].year, json.chapters[chapter].events[event].dates[date].month, json.chapters[chapter].events[event].dates[date].day);
+		}
+		else {
+		    dateEnd = new Date(json.chapters[chapter].events[event].dates[date].year, json.chapters[chapter].events[event].dates[date].month, json.chapters[chapter].events[event].dates[date].day);
+		}
+            }
+	    if(dateEnd === null) {
+                //items.add({id: id, content: text, start: dateStart, group: group });
+                items.add({id: id, content: text, start: dateStart });
+	    }
+	    else {
+		//items.add({id: id, content: text, start: dateStart, end: dateEnd, group: group});
+		items.add({id: id, content: text, start: dateStart, end: dateEnd });
+	    }
+	    id = id+1;
+            text = "";
+            dateStart = null;
+	    dateEnd = null;
+        }
+    }
+/*
     // Create a DataSet (allows two way data-binding)
     var items = new vis.DataSet([
         {id: 1, content: 'Demo Date', start: new Date(-4000, 04, 20)},
-        {id: 2, content: 'Demo Date', start: new Date(-500, 04, 14)},
-        {id: 3, content: 'Demo Date', start: new Date(1600, 04, 21)},
-        {id: 4, content: 'Demo Period', start: new Date(-200, 02, 10), end: new Date(200, 01, 09)}
-    ]);
+*/
 
 // Configuration for the Timeline
     var options = {};
