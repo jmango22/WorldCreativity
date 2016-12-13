25e787a21d47cfa971a2005e5813d985db7e6460
mengje <MengJE@stash.mtc.byu.edu>
2016-12-13 03:22:04 -0700

    fixed it up

--- timeline.js
+++ timeline.js
@@ -3,68 +3,59 @@
  */
 
 google.charts.load('upcoming', {'packages':['geochart']});
-//google.charts.setOnLoadCallback(drawRegionsMap(null));
+//google.charts.setOnLoadCallback(drawBlankMap());
 google.charts.setOnLoadCallback(drawTimeLine);
 
+var data = new google.visualization.DataTable();
+var options = {};
 
-function drawRegionsMap(array) {
-    var data;
-
-    if(array === null) {
-        data = new new google.visualization.DataTable();
-    }
-    else {
-        data = google.visualization.arrayToDataTable(array);
-    }
-
-    var options = {};
-
+function drawRegionsMap(data, options) {
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    self.data = data;
+    self.options = options;
 
     chart.draw(data, options);
 }
 
-var options = {};
 
-var timelineItems = new vis.DataSet(options);
 
 function drawTimeLine() {
     // DOM element where the Timeline will be attached
     var container = document.getElementById('visualization');
 
+    var options = {};
+
+    var timelineItems = new vis.DataSet(options);
+
     var id = 1;
     var dateStart = null;
     var dateEnd = null;
-    var group = 0;
 
-    for (var chapter in json.chapters)
+    for (var event in json)
     {
-        //console.log(json.chapters[chapter].title);
-	    group = group+1;
-        for ( var event in json.chapters[chapter].events )
-        {
-            //console.log("  "+json.chapters[chapter].events[event].name);
-	        text = json.chapters[chapter].events[event].name;
-            for( var date in json.chapters[chapter].events[event].dates) {
-                //console.log("    "+json.chapters[chapter].events[event].dates[date].month+"/"+json.chapters[chapter].events[event].dates[date].day+"/"+json.chapters[chapter].events[event].dates[date].year);
-		        if(dateStart === null) {
-		            dateStart = new Date(json.chapters[chapter].events[event].dates[date].year, json.chapters[chapter].events[event].dates[date].month, json.chapters[chapter].events[event].dates[date].day);
-		        }
-		        else {
-		            dateEnd = new Date(json.chapters[chapter].events[event].dates[date].year, json.chapters[chapter].events[event].dates[date].month, json.chapters[chapter].events[event].dates[date].day);
-		        }
+        console.log(json[event].name);
+        for( var date in json[event].dates) {
+            console.log("    "+json[event].dates[date].month+"/"+json[event].dates[date].day+"/"+json[event].dates[date].year);
+
+            if(dateStart === null) {
+                dateStart = new Date(json[event].dates[date].year, json[event].dates[date].month, json[event].dates[date].day);
+                dateStart.setFullYear(json[event].dates[date].year);
             }
-	        if(dateEnd === null) {
-                timelineItems.add({id: id, content: text, start: dateStart });
-	        }
-	        else {
-		        timelineItems.add({id: id, content: text, start: dateStart, end: dateEnd });
-	        }
-	        id = id+1;
-            text = "";
-            dateStart = null;
-	        dateEnd = null;
+            else {
+                dateEnd = new Date(json[event].dates[date].year, json[event].dates[date].month, json[event].dates[date].day);
+                dateEnd.setFullYear(json[event].dates[date].year);
+            }
         }
+        if(dateEnd === null) {
+            timelineItems.add({id: id, content: json[event].name, start: dateStart });
+        }
+        else {
+            timelineItems.add({id: id, content: json[event].name, start: dateStart, end: dateEnd });
+        }
+        id = id+1;
+        dateStart = null;
+        dateEnd = null;
     }
 
 // Configuration for the Timeline
@@ -74,41 +65,46 @@ function drawTimeLine() {
     var timeline = new vis.Timeline(container, timelineItems, options);
 
     timeline.on('select', function (properties) {
-        //console.log('selected items: ' + properties.items);
-        //console.log(timelineItems.get(properties.items)[0].content);
 
-        var arr = [];
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Lat');
+        data.addColumn('number', 'Long');
+        data.addColumn('string', 'name');
+        data.addColumn({type: 'string', role:'tooltip'});
 
-        for (var chapter in json.chapters)
+        var options = {};
+
+        for (var event in json)
         {
-            //console.log(json.chapters[chapter].title);
-            group = group+1;
-            for ( var event in json.chapters[chapter].events )
-            {
-                //console.log("  "+json.chapters[chapter].events[event].name);
-                text = json.chapters[chapter].events[event].name;
-                if(text === timelineItems.get(properties.items)[0].content) {
-                    console.log(text);
-                    for (var country in json.chapters[chapter].events[event].locations.countries) {
-                        console.log(json.chapters[chapter].events[event].locations.countries[country]);
-
-                        if(arr.size === 0) {
-                            arr.push(['Country', 'Event']);
-                        }
-
-                        arr.push([json.chapters[chapter].events[event].locations.countries[country], text]);
-
-                    }
-                    for (var city in json.chapters[chapter].events[event].locations.cities) {
-                        console.log(json.chapters[chapter].events[event].locations.cities[city]);
-                    }
+            if(json[event].name === timelineItems.get(properties.items)[0].content) {
+                console.log(json[event].name);
+                for (var city in json[event].cities) {
+                    console.log(json[event].cities[city]);
+                    options = {
+                        region: json[event].region,
+                        displayMode: 'markers'
+                    };
+                    data.addRows([[json[event].cities[city].Lat, json[event].cities[city].Long, json[event].cities[city].name, json[event].details]]);
                 }
             }
         }
-        drawRegionsMap(arr);
 
-
+        drawRegionsMap(data, options);
     });
+}
+
+function fit() {
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(self.data, self.options);
+}
+
+window.onresize = function () {
+    fit();
+}
+
+window.onload = function () {
+    fit();
 }
 
 
